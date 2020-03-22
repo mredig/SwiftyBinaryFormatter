8af8557b67d60393f24b027b8925227ff6a811ae
Michael Redig <mredig@gmail.com>
2020-03-22 18:34:21 -0500

    (feat) extended Data to have functionality of BinaryFormatter
    deprecated BinaryFormatter

--- SwiftyBinaryFormatter/BinaryFormatter.swift
+++ SwiftyBinaryFormatter/BinaryFormatter.swift
@@ -15,7 +15,7 @@ public enum BinaryErrors: Error {
 	case nonAsciiCharacter
 }
 
-extension BinaryFormatter.LongWord: BinaryFormattingProtocol {
+extension Data.LongWord: BinaryFormattingProtocol {
 	public init(withDoubleRepresentation double: Double) {
 		self.init(double.bitPattern)
 	}
@@ -25,28 +25,28 @@ extension BinaryFormatter.LongWord: BinaryFormattingProtocol {
 	}
 }
 
-extension BinaryFormatter.Word: BinaryFormattingProtocol {
+extension Data.Word: BinaryFormattingProtocol {
 	public init(withFloatRepresentation float: Float) {
 		self.init(float.bitPattern)
 	}
 }
 
-extension BinaryFormatter.TwoByte: BinaryFormattingProtocol {}
+extension Data.TwoByte: BinaryFormattingProtocol {}
 
-extension BinaryFormatter.Byte: BinaryFormattingProtocol {}
+extension Data.Byte: BinaryFormattingProtocol {}
 
 
+@available(*, deprecated, renamed: "Data")
 public struct BinaryFormatter {
-
 	public typealias LongWord = UInt64
 	public typealias Word = UInt32
 	public typealias TwoByte = UInt16
 	public typealias Byte = UInt8
 
+
 	public var byteCount: Int { data.count }
 
 	public private(set) var data: Data
-
 
 	public init() {
 		data = Data()
@@ -61,15 +61,15 @@ public struct BinaryFormatter {
 	}
 
 	public mutating func append(element: BinaryFormattingProtocol) {
-		data.append(contentsOf: element.bytes)
+		data.append(element: element)
 	}
 
 	public mutating func append(formatter: BinaryFormatter) {
-		data.append(contentsOf: formatter.data)
+		data.append(formatter: formatter)
 	}
 
 	public mutating func append(sequence: [BinaryFormattingProtocol]) {
-		data.append(contentsOf: sequence.flatMap { $0.bytes })
+		data.append(sequence: sequence)
 	}
 
 	public subscript(index: Int) -> BinaryFormatter.Byte {
@@ -81,10 +81,47 @@ public struct BinaryFormatter {
 		}
 	}
 
-	public var hexString: String {
-		data.reduce("") { $0 + $1.hexString }
-	}
+	public var hexString: String { data.hexString }
 
 	@available(*, deprecated, renamed: "data")
 	public var renderedData: Data { data }
+}
+
+
+extension Data {
+	public typealias LongWord = UInt64
+	public typealias Word = UInt32
+	public typealias TwoByte = UInt16
+	public typealias Byte = UInt8
+
+	var hexString: String {
+		reduce("") { $0 + $1.hexString }
+	}
+
+	@available(*, deprecated, renamed: "count")
+	var byteCount: Int { count }
+
+	@available(*, deprecated, message: "No need to use the `data` property as this is an instance of Data itself")
+	var data: Data { self }
+
+	public init(data: [BinaryFormattingProtocol]) {
+		self.init(data.flatMap { $0.bytes })
+	}
+
+	public init(data: [Data.Byte]) {
+		self.init(data)
+	}
+
+	mutating func append(element: BinaryFormattingProtocol) {
+		append(contentsOf: element.bytes)
+	}
+
+	@available(*, deprecated, message: "BinaryFormatter is deprecated. Look into migrating the source to Data")
+	mutating func append(formatter: BinaryFormatter) {
+		append(contentsOf: formatter.data)
+	}
+
+	mutating func append(sequence: [BinaryFormattingProtocol]) {
+		append(contentsOf: sequence.flatMap { $0.bytes })
+	}
 }
